ffc282c546b8f228af3d793313e42a99208d418f
georgesipp <georgesipp@gmx.de>
2024-09-11 20:44:30 +0200

    ka

--- svelte.config.js
+++ svelte.config.js
@@ -8,6 +8,9 @@ const dev = process.argv.includes('dev');
 const config = {
 	kit: {
 		adapter: adapter(),
+		paths: {
+			base: process.argv.includes('dev') ? '' : process.env.BASE_PATH
+		}
 
 	}
 };
